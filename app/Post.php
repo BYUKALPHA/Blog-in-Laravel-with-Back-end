0e06cf2e39b13407beae736d55054b59eb222f2e
Byukusenge Alphonse <alphonso2009@gmail.com>
2020-02-07 14:11:03 +0300

    Crate new migration to generate new comments table

--- app/Post.php
+++ app/Post.php
@@ -31,6 +31,10 @@ class Post extends Model
     {
         return $this->belongsToMany(Tag::class);
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
 
     public function getImageUrlAttribute($value)
